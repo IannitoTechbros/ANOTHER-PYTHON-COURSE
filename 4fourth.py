74cb5aeb997360b74f466e1aa85a9d3c775aafa7
IannitoTechbros <ianmutugi91@gmail.com>
2025-06-11 17:37:54 +0300

    fourth-lesson

---
+++ 4fourth.py
@@ -0,0 +1,15 @@
+import math
+
+print(2 ** 3)
+
+
+number1 = 45
+
+number2 = 78
+
+
+number3 = math.ceil(number1 / number2)
+number4 = math.floor(number1 / number2)
+
+print(number3)
+print(number4)
