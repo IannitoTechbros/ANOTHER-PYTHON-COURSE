d061740eb25863e8e1acd40ba3dce1bc7f8b5112
IannitoTechbros <ianmutugi91@gmail.com>
2025-06-11 19:10:01 +0300

    sixth-lesson

--- 5fifth.py
+++ 5fifth.py
@@ -3,3 +3,12 @@
 result1 = (20 + 3 * 4)
 
 print(result1)
+
+
+#There is a data type called complex
+complex
+ #returns binary equivalent
+
+print(bin(result1))
+
+print(int("0b100000", 2))
---
+++ 6.sixth.py
@@ -0,0 +1,27 @@
+#   variables
+
+iq = 91
+
+
+print(bin(iq))
+
+print(iq)
+
+#need to use snake_case
+#start with lowercase
+#case_ sensitive
+#don't overwite keywords
+
+
+prime_number = 2
+
+Prime_number = 4
+
+# variales can be assigned
+
+
+
+#constants
+
+PI = 3.14
+
