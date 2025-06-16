9b732b610589a452cc8b69cfc25805332c14ad0e
IannitoTechbros <ianmutugi91@gmail.com>
2025-06-16 15:52:13 +0300

    ninth-class

---
+++ 9ninth.py
@@ -0,0 +1,32 @@
+#lets talk about strings
+
+first_string = "Hi, hello there"
+
+print(type(first_string))
+
+username = 'supercoder'
+
+password = 'supersecret'
+
+long_string = '''
+
+W O W
+
+O   O 
+
+_____
+
+'''
+
+print(long_string)
+
+
+
+first_name= 'Andrei'
+
+last_name = "Negoi"
+
+full_name = first_name + " " +  last_name
+
+print(full_name)
+
