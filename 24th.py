aeef403322569eb90ef4e57e63aed8ffe42880cd
IannitoTechbros <ianmutugi91@gmail.com>
2025-06-18 16:35:25 +0300

    24th lesson dict

---
+++ 24th.py
@@ -0,0 +1,28 @@
+#dictionary
+
+dictionary = {
+    "username" : "Iannito",
+    "password" : "Iannito2",
+    "age" : 21,
+    "likes" : ["Driving", "Kissing", "Hugging"]
+}
+
+
+print(dictionary["username"])
+
+
+my_list = [
+    {
+    "username" : "Iannito",
+    "password" : "Iannito2",
+    "age" : 21,
+    "likes" : ["Driving", "Kissing", "Hugging"]
+    },
+    {
+    "username" : "Moreen",
+    "password" : "Moreen2",
+    "age" : 21,
+    "likes" : ["Indian", "Running", "Hugging"]
+    }
+
+]
